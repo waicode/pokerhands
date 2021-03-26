89424ed59511ec5c4a31a97a7d38992808865f72
waicode <waicode37@gmail.com>
2021-03-26 13:30:36 +0900

    fix: rollback

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,6 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
-const { webpackConfig, merge } = require('@rails/webpacker')
 const environment = require('./environment')
 
-module.exports = merge(webpackConfig, environment)
+module.exports = environment.toWebpackConfig()
